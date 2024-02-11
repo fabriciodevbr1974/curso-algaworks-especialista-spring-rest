42627a36d92b79e54ec8c1580575af86169219cb
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-11 18:26:41 -0300

    Testando @Component

--- workspace/algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ workspace/algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -7,10 +7,6 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail {
 	
-	public NotificadorEmail() {
-		System.out.println("Construtor chamado");
-	}
-	
 	public void notificar(Cliente cliente, String mensagem) {
 		System.out.printf("Notificando %s atrav�s do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
