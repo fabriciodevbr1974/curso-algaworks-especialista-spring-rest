56333619a502b62155fe6d0c27323f9d454b2f96
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-11 22:23:23 -0300

    Aula 2.14 >> Ponto de injeção setter @Autowired

--- workspace/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ workspace/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -11,21 +11,23 @@ public class AtivacaoClienteService {
 
 	private Notificador notificador;
 
-	@Autowired //Ponto de injeção no construtor
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-	}
-	
-	public AtivacaoClienteService(String qualquer) {
-		this.notificador = notificador;
-	}
-	
-	
-	
+//	@Autowired //Ponto de injeção no construtor
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+
+//	public AtivacaoClienteService(String qualquer) {
+//		this.notificador = notificador;
+//	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		this.notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+	}
+
+	@Autowired //Ponto de injeção no set
+	public void setNotificador(Notificador notificador) {
+		this.notificador = notificador;
 	}
 
 }
