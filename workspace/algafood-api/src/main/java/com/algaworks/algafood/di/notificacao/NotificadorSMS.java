bc4876f3ee8906ce0efb5dd993ea826687568106
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-11 22:49:33 -0300

    Aula 2.17 >> Desambiguação com @Primary

--- workspace/algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
+++ workspace/algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
@@ -1,15 +1,18 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
+@Primary
 @Component
 public class NotificadorSMS implements Notificador {
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.printf("Notificando %s por SMS através do telefone %s: %s\n", cliente.getNome(), cliente.getTelefone(), mensagem);
+		System.out.printf("Notificando %s por SMS através do telefone %s: %s\n", cliente.getNome(),
+				cliente.getTelefone(), mensagem);
 	}
 
 }
