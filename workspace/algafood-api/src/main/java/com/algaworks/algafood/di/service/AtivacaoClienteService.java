b6b5b29cca063dcf2ae92af5879538a8c734697f
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-11 22:20:31 -0300

    Aula 2.14 >> Ponto de injeção no construtor @Autowired

--- workspace/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ workspace/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,16 +1,27 @@
 package com.algaworks.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 
 	private Notificador notificador;
 
+	@Autowired //Ponto de injeção no construtor
 	public AtivacaoClienteService(Notificador notificador) {
 		this.notificador = notificador;
-		System.out.println("AtivacaoClienteService: " + notificador);
 	}
+	
+	public AtivacaoClienteService(String qualquer) {
+		this.notificador = notificador;
+	}
+	
+	
+	
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
