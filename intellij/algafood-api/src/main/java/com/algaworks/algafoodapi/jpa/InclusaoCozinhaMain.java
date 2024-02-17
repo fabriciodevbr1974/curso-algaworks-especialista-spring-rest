45ab6002b768447b2d5d2731a590ec4b4a8fc1a7
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-17 16:21:18 -0300

    Aula 3.9. Adicionando um objeto no banco de dados

--- intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/InclusaoCozinhaMain.java
+++ intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/InclusaoCozinhaMain.java
@@ -6,9 +6,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-import java.util.List;
-
-public class ConsultaCozinhaMain {
+public class InclusaoCozinhaMain {
 
 
   public static void main(String[] args) {
@@ -19,15 +17,20 @@ public class ConsultaCozinhaMain {
 
     CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-    List<Cozinha> cozinhas = cadastroCozinha.listar();
+    Cozinha cozinha1 = new Cozinha();
+    cozinha1.setNome("Chinesa");
 
-    for (Cozinha cozinha : cozinhas) {
-      System.out.println(cozinha.getNome());
-    }
+    Cozinha cozinha2 = new Cozinha();
+    cozinha2.setNome("Peruana");
+
+    cozinha1 = cadastroCozinha.adicionar(cozinha1);
+    cozinha2 = cadastroCozinha.adicionar(cozinha2);
+
+    System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
+    System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+
+
   }
-
-
-
 
 
 }
