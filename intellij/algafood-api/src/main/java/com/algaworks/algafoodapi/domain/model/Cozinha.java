9958f6096da7c8ceb97d9eade68fe1af11b2b3ea
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-17 18:53:22 -0300

    3.19. Propriedade nullable de @Column e @JoinColumn

--- intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
+++ intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
@@ -1,9 +1,6 @@
 package com.algaworks.algafoodapi.domain.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 
@@ -13,7 +10,7 @@ public class Cozinha {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
+  @Column(nullable = false)
   private String nome;
 
 
