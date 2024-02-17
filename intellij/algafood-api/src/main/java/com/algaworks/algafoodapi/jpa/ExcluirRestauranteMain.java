7656d4f457017ed6f712ca66d0677f1470e60e54
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-17 18:10:57 -0300

    3.16. Desafio: Lombok e repositório de restaurantes

---
+++ intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/ExcluirRestauranteMain.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafoodapi.jpa;
+
+import com.algaworks.algafoodapi.AlgafoodApiApplication;
+import com.algaworks.algafoodapi.domain.model.Restaurante;
+import com.algaworks.algafoodapi.domain.repository.RestauranteRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class ExcluirRestauranteMain {
+
+
+  public static void main(String[] args) {
+
+    ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+            .web(WebApplicationType.NONE)
+            .run(args);
+
+    RestauranteRepository restauranteRepository = applicationContext.getBean(RestauranteRepository.class);
+
+    Restaurante restaurante = new Restaurante();
+    restaurante.setId(2L);
+    restauranteRepository.remover(restaurante);
+
+
+
+
+  }
+
+
+}
