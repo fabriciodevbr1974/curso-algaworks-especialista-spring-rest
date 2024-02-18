d3f05ea5be6f895a196a64ed5a75805e9b5e1b4f
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-18 16:14:50 -0300

    4.23. Modelando e implementando a inclusão de recursos com POST

--- intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
+++ intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -4,7 +4,6 @@ import com.algaworks.algafoodapi.api.controller.model.Cozinha;
 import com.algaworks.algafoodapi.api.controller.model.CozinhaXmlWrapper;
 import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,20 +19,20 @@ public class CozinhaController {
   private CozinhaRepository cozinhaRepository;
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public List<Cozinha> listarJSON(){
+  public List<Cozinha> listarJSON() {
     return cozinhaRepository.listar();
   }
 
   @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-  public CozinhaXmlWrapper listarXML(){
+  public CozinhaXmlWrapper listarXML() {
     return new CozinhaXmlWrapper(cozinhaRepository.listar());
   }
 
   @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Cozinha> buscarJSON(@PathVariable Long id){
+  public ResponseEntity<Cozinha> buscarJSON(@PathVariable Long id) {
 
     Cozinha cozinha = cozinhaRepository.buscar(id);
-    if(cozinha != null){
+    if (cozinha != null) {
       return ResponseEntity.ok(cozinha);
     }
 
@@ -43,9 +42,23 @@ public class CozinhaController {
   }
 
   @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_XML_VALUE)
-  public Cozinha buscarXML(@PathVariable Long id){
-    return cozinhaRepository.buscar(id);
+  public ResponseEntity<Cozinha> buscarXML(@PathVariable Long id) {
+    Cozinha cozinha = cozinhaRepository.buscar(id);
+
+    if (cozinha != null) {
+      return ResponseEntity.ok(cozinha);
+    }
+
+//    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    return ResponseEntity.notFound().build();
+
+
   }
 
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Cozinha adicionar(@RequestBody Cozinha cozinha){
+    return cozinhaRepository.salvar(cozinha);
+  }
 
 }
