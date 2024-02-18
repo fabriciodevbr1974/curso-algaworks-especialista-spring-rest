71b35d59b921090a8dd9830b5a55936bbb4952e7
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-18 17:09:39 -0300

    4.27. Implementando a camada de domain services (e a importância da linguagem ubíqua)

--- intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
+++ intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafoodapi.api.controller;
 import com.algaworks.algafoodapi.api.controller.model.Cozinha;
 import com.algaworks.algafoodapi.api.controller.model.CozinhaXmlWrapper;
 import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
+import com.algaworks.algafoodapi.domain.service.CadastroCozinhaService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -19,6 +20,9 @@ public class CozinhaController {
 
   @Autowired
   private CozinhaRepository cozinhaRepository;
+
+  @Autowired
+  private CadastroCozinhaService cadastroCozinhaService;
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public List<Cozinha> listarJSON() {
@@ -60,7 +64,7 @@ public class CozinhaController {
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public Cozinha adicionar(@RequestBody Cozinha cozinha) {
-    return cozinhaRepository.salvar(cozinha);
+    return cadastroCozinhaService.salvar(cozinha);
   }
 
   @PutMapping("/{id}")
