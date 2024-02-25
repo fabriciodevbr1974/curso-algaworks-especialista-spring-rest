2061ead2746724b9683ca00c482c2b511656059a
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-25 16:48:04 -0300

    4.31. Desafio: Modelando e implementando a atualização de recursos de restaurantes

--- intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
+++ intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
@@ -4,6 +4,7 @@ import com.algaworks.algafoodapi.api.controller.model.Restaurante;
 import com.algaworks.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafoodapi.domain.repository.RestauranteRepository;
 import com.algaworks.algafoodapi.domain.service.CadastroRestauranteService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,8 +38,35 @@ public class RestauranteController {
   }
 
   @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
   public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante){
+
+    try {
+      restaurante = cadastroRestauranteService.salvar(restaurante);
+      return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+    } catch (EntidadeNaoEncontradaException e){
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+  @PutMapping("/{restauranteId}")
+  public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante){
+
+
+    Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+    if(restauranteAtual == null){
+      return ResponseEntity.notFound().build();
+    }
+
+    if(restauranteAtual != null){
+      BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+
+      try {
+        restauranteAtual = cadastroRestauranteService.salvar(restauranteAtual);
+        return ResponseEntity.ok(restauranteAtual);
+      } catch (EntidadeNaoEncontradaException e){
+        return ResponseEntity.badRequest().body(e.getMessage());
+      }
+    }
 
     try {
       restaurante = cadastroRestauranteService.salvar(restaurante);
