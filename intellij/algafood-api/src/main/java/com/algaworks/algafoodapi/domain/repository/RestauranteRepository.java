a9d6dc9fb4802fd3bd0ed4dbea426b64ddad4afb
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-03-05 20:52:59 -0300

    5.9. Usando queries JPQL customizadas com @Query

--- intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/repository/RestauranteRepository.java
+++ intellij/algafood-api/src/main/java/com/algaworks/algafoodapi/domain/repository/RestauranteRepository.java
@@ -2,8 +2,9 @@ package com.algaworks.algafoodapi.domain.repository;
 
 import com.algaworks.algafoodapi.api.controller.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -13,8 +14,14 @@ import java.util.Optional;
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
   List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+  @Query("from Restaurante where nome = %:nome% and cozinha.id = :id")
+  List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+
   List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+
   Optional<Restaurante> findFirstByNomeContaining(String nome); //Retorna o primeiro registro da lista (findFirst).
+
   List<Restaurante> findTop2ByNomeContaining(String nome);//Retoran os dois primeiros registros da lista (findTop2).
 
   int countByCozinhaId(Long cozinha);
